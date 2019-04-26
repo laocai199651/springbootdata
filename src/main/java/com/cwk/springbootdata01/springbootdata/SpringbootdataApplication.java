9ceaa4b5bf3adb3c45b9b4591a67451182a15258
dsg005 <dsg005@com>
2019-04-26 23:57:53 +0800

    Initial commit

---
+++ src/main/java/com/cwk/springbootdata01/springbootdata/SpringbootdataApplication.java
@@ -0,0 +1,13 @@
+package com.cwk.springbootdata01.springbootdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cwk/springbootdata01/springbootdata/SpringbootdataApplicationTests.java
@@ -0,0 +1,31 @@
+package com.cwk.springbootdata01.springbootdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootdataApplicationTests {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException {
+        logger.info("dataSource\t" + dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        logger.info("connection\t" + connection);
+    }
+
+}
